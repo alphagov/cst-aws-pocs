b4807df607de26f1a3afcac2996b5e747aaff727
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2018-08-08 12:22:16 +0100

    Applied formatting corrections as per Flake8 spec

--- boto3-assume/run.py
+++ boto3-assume/run.py
@@ -1,29 +1,28 @@
-# Test script to assume into a different AWS account using sts assume-role 
-# keep those session credentials in memory and then invoke api calls 
-# across 2 AWS endpoints using those credentials. 
+# Test script to assume into a different AWS account using sts assume-role
+# keep those session credentials in memory and then invoke api calls
+# across 2 AWS endpoints using those credentials.
 
-# takes 2 command line options 
+# takes 2 command line options
 # --email=<your-email> or -e <your-email>
 # --token=<your-mfa-token> or -t <your-mfa-token>
 # if either option are not supplied the script will prompt for them
 
-# at present it just lists the number of S3 buckets and the name 
-# of the first bucket it finds 
+# at present it just lists the number of S3 buckets and the name
+# of the first bucket it finds
 
-# it then lists all the ingress and egresss cidrs for security 
+# it then lists all the ingress and egresss cidrs for security
 # groups in all regions specified in the regions array
 
 from aws.gds_aws_client import GdsAwsClient
 from aws.gds_s3_client import GdsS3Client
 from aws.gds_ec2_client import GdsEc2Client
 from aws.gds_support_client import GdsSupportClient
-import json
 from optparse import OptionParser
 
 
 # allow user to supply email and token as script options
 parser = OptionParser()
-parser.add_option("-e", "--email", dest="email", default="", 
+parser.add_option("-e", "--email", dest="email", default="",
                   help="Your gov.uk email")
 parser.add_option("-t", "--token", dest="token", default="",
                   help="Your MFA auth token")
@@ -33,66 +32,66 @@ parser.add_option("-t", "--token", dest="token", default="",
 
 
 # get the user details from stdin if not specified
-if options.email == "": 
+if options.email == "":
     options.email = input("Email? ")
 
 if options.token == "":
     options.token = input("MFA? ")
 
 # temp hard code existing account and role name
-regions = ["eu-west-1","eu-west-2"]
+regions = ["eu-west-1", "eu-west-2"]
 account = "779799343306"
 role = "AdminRole"
 
-### Assume Role ###
+# Assume Role
 aws_client = GdsAwsClient()
-# assume role into target account 
-assumed = aws_client.assume_role(account,role,options.email,options.token)
-session = aws_client.get_session(account,role)
+# assume role into target account
+assumed = aws_client.assume_role(account, role, options.email, options.token)
+session = aws_client.get_session(account, role)
 
 print(session['AccessKeyId'])
 
-### S3  ###
-print ("### S3  ###")
+# S3
+print("### S3  ###")
 s3_client = GdsS3Client()
 
-# retrieve bucket list 
+# retrieve bucket list
 buckets = s3_client.get_bucket_list(session)
 # output number of buckets
 print("Found " + str(len(buckets)) + " buckets:\n")
 
-for bucket in buckets: 
+for bucket in buckets:
 
     # output bucket name
     print("Bucket name: " + bucket['Name'] + "\n")
     policy = s3_client.get_bucket_policy(session, bucket['Name'])
     print("Policy: " + policy + "\n")
 
-### EC2 ###
-print ("### EC2 ###")
+# EC2
+print("### EC2 ###")
 ec2_client = GdsEc2Client()
 
 # describe security groups
 for region in regions:
-    
+
     print(f"#### REGION: {region} ####")
     groups = ec2_client.describe_security_groups(session, region)
 
     for group in groups:
 
         print(group['GroupName'])
-        
+
         print(" ingress ")
         for perm in groups[0]['IpPermissions']:
-            
-            if 'FromPort' in perm.keys(): 
+
+            if 'FromPort' in perm.keys():
                 from_port = perm['FromPort']
-            else: 
+            else:
                 from_port = 'Anywhere'
 
             if 'ToPort' in perm.keys():
                 to_port = perm['ToPort']
-            else: 
+            else:
                 to_port = 'Anywhere'
 
             ports = f"{from_port}>{to_port}"
@@ -101,23 +100,22 @@ for region in regions:
                 cidr_range = cidr['CidrIp']
                 if 'Description' in cidr.keys():
                     cidr_desc = cidr['Description']
-                else: 
+                else:
                     cidr_desc = "No description"
-                    
-                print(f"{ports} = {cidr_range}: {cidr_desc}")
 
+                print(f"{ports} = {cidr_range}: {cidr_desc}")
 
         print(" egress ")
         for perm in groups[0]['IpPermissionsEgress']:
-            
-            if 'FromPort' in perm.keys(): 
+
+            if 'FromPort' in perm.keys():
                 from_port = perm['FromPort']
-            else: 
+            else:
                 from_port = 'Anywhere'
 
             if 'ToPort' in perm.keys():
                 to_port = perm['ToPort']
-            else: 
+            else:
                 to_port = 'Anywhere'
 
             ports = f"{from_port}>{to_port}"
@@ -126,33 +124,31 @@ for region in regions:
                 cidr_range = cidr['CidrIp']
                 if 'Description' in cidr.keys():
                     cidr_desc = cidr['Description']
-                else: 
+                else:
                     cidr_desc = "No description"
 
                 print(f"{ports} = {cidr_range}: {cidr_desc}")
 
-
-### Support ###
-print ("### Support ###")
+# Support
+print("### Support ###")
 support_client = GdsSupportClient()
 
-# retrieve bucket list 
+# retrieve bucket list
 checks = support_client.describe_trusted_advisor_checks(session)
-#
-for check in checks: 
-    
+
+for check in checks:
 
     result = support_client.describe_trusted_advisor_check_result(session, check['id'])
     if 'resourcesSummary' in result.keys():
 
-        if result['resourcesSummary']['resourcesProcessed'] > 0: 
+        if result['resourcesSummary']['resourcesProcessed'] > 0:
             print(check['id'])
             print("check name: " + check['name'] + "\n")
             print("description: " + check['description'] + "\n")
             print("category: " + check['category'] + "\n   stats:\n")
 
-            print ("processed: " + str(result['resourcesSummary']['resourcesProcessed']))
-            print ("flagged: " + str(result['resourcesSummary']['resourcesFlagged']))
-            print ("ignored: " + str(result['resourcesSummary']['resourcesIgnored']))
-            print ("supressed: " + str(result['resourcesSummary']['resourcesSuppressed']))
-            print ("\n")
+            print("processed: " + str(result['resourcesSummary']['resourcesProcessed']))
+            print("flagged: " + str(result['resourcesSummary']['resourcesFlagged']))
+            print("ignored: " + str(result['resourcesSummary']['resourcesIgnored']))
+            print("supressed: " + str(result['resourcesSummary']['resourcesSuppressed']))
+            print("\n")
